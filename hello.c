096627e4db08c5ea2b7a1c3ce52b5a6cb6af908d
unknown <2020a1r117@mietjammu.in>
2021-03-19 20:05:35 +0530

    hello world file added

---
+++ hello.c
@@ -0,0 +1,9 @@
+//Author name: hrithik koul
+//Creation Date: 16-March-2021
+//Purpose: Hello World Program
+#include<stdio.h>//preprocessor directive to include standard input output function header file
+int main(){//main function body start
+
+	printf("Hello Word");//printf statment to print "Hello World".
+	return 0;            //return statement
+}//main function body ends
