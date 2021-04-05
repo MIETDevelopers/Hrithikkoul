61efa18673f0ef1c0c3f7192b1afa621fafaccb5
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 11:28:56 +0530

    file is added

--- 15_Armstrong.c
+++ 15_Armstrong.c
@@ -1,26 +1,28 @@
-//© Ishav Verma 18/March/2021
-#include <stdio.h>
-int main() {
-  int num, originalNum, remainder, result = 0;
-  printf("Enter a three-digit integer: \n");
-  scanf("%d", &num);//It will scan the operater entered by user.
+//Author name:Hrithik koul
+//creation date: 20-03-2021
+// purpose:Check no. is armstrong or not using while loop
+#include <stdio.h>//Preprocessor directive to include input output function header file
+int main(){//Start of main function body
+  int num, originalNum, remainder, sum = 0;//Declaring variables of integer data type
+  printf("Enter a three-digit integer: \n");//Printf function calling to print a three digit integer
+  scanf("%d", &num);//Scanf function calling to input the value given by user 
   originalNum = num;
-
+//start of while loop
   while (originalNum != 0) {
      //Formula
      // remainder contains the last digit
     remainder = originalNum % 10;
         
-    result += remainder * remainder * remainder;
+    sum+= remainder * remainder * remainder;
         
        // removing last digit from the orignal number
     originalNum /= 10;
   }
 
-  if (result == num)
-      printf("%d is an Armstrong number.", num);
-  else
-      printf("%d is not an Armstrong number.", num);
+  if (sum == num)//if statement
+      printf("%d is an Armstrong number.", num);//printf function calling to print an armstrong number
+  else//else statement
+      printf("%d is not an Armstrong number.", num);//printf function calling to print not an armstrong number
 
-  return 0;
-}
+  return 0;//return statement
+}//End of main function body
