98711f8c9d299947e55a012030185c9618fdc820
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 12:17:33 +0530

    file is added

--- 25_SwapCallByValue.c
+++ 25_SwapCallByValue.c
@@ -1,17 +1,30 @@
-//A C Program to swap two numbers using call by value.              © Ishav Verma 31/March/2021
-#include<stdio.h>
-void swap(int, int);
-int main(){
-    int a, b;
-    printf("Enter two values: ");
-    scanf("%d %d", &a, &b);//Taking input from user.
-    swap(a,b);
+// Author Name :Hrithik koul
+// Creation Date: 21-03-2021
+//Purpose: Call by Value Example - Swapping 2 numbers using Call by Value
+#include <stdio.h>
+void swap(int , int ); //  variable to pass by reference (which is the the address of an integer variable)
+ 
+int main() // Main Body 
+{
+   int x, y;// Variable Declration 
+ 
+   printf("Enter the value of x and y\n"); // Input from user
+   scanf("%d%d",&x,&y);
+ 
+   printf("Before Swapping\nx = %d\ny = %d\n", x, y); // Before Swapping 
+ 
+   swap(x, y); 
+ 
+   printf("After Swapping\nx = %d\ny = %d\n", y, x); // After Swapping 
+ 
+   return 0;
 }
-//Here swapping exists.
-void swap (int a, int b){
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-    printf("%d %d", a, b);
-}
+ 
+void swap(int a, int b) // variable to pass by reference
+{
+   int temp; // Variable Declaration 
+ 
+   temp = b;
+   b = a;
+   a = temp;
+    printf("Values of a and b is %d  %d\n",a,b); // Printing Statement 
