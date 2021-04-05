bbbd694544d6892eae8e21ca6aa4419cb5405517
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 11:16:45 +0530

    file is added

--- 11_Fibonacci.c
+++ 11_Fibonacci.c
@@ -1,25 +1,31 @@
-#include<stdio.h>
+//Author name: Hrithik Koul
+//Creation Date:17-03-2021
+//purpose:Fibonacci sequence using for loop
+#include<stdio.h>//Preprocessor directive to include input output function header file
+int main (){//start of the main body function 
  
-int main(){
-   int n, first = 0, second = 1, next, c; //For the reference to the formuls of the series.
+  int first = 0, second = 1, third, i, n=50;//value declaration and assignment of integer data type
  
-   printf("Enter the number of terms\n");
-   scanf("%d",&n); //This will scan (read) the number entered by user.
+  printf ("Enter the length of the fibonacci series \n");//prinf calling to print Enter the length of the fibonacci series
  
-   printf("First %d terms of Fibonacci series are :-\n",n);
+  scanf ("%d", &n);//scanf function calling to input user value in n
  
-   for ( c = 0 ; c < n ; c++ ) //ForLoop function for the reference to the formuls of the series.
-   {
-      if ( c <= 1 )
-         next = c;
-      else
-      {
-         //Formula to print the series.
-         next = first + second;
-         first = second;
-         second = next;
-      }
-      printf("%d\n",next); //This will give the outpu.
-   }
-   return 0;
-}
+  printf ("The Fibonacci series is :\n");//prinf calling to print The fibonacci series is 
+ 
+  printf ("%d%d", first, second);//printf function calling to print value first second
+ 
+  for (i = 2; i <=n; i++){//for loop body start
+
+      third = first + second;//logic to get fibonacci sequence
+ 
+      printf ("%d ", third);//printf function calling to print third
+ 
+      first = second;//first equal to second
+ 
+      second = third;//second equal to third
+ 
+    }//for loop body end
+ 
+  return 0;//return statement
+ 
+}//main body function ends
