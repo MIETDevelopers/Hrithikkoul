632ffa68b298648c50b66cd6d8edaa1949f0556c
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 12:34:54 +0530

    file is added

--- 28_StudentUnion.c
+++ 28_StudentUnion.c
@@ -1,38 +1,38 @@
-//A C Program to store information of a student using union.          © Ishav Verma 20/March/2021
-#include <stdio.h>
-#include <string.h>
+//Author Name:Hrithik koul
+//Creation Date: 21th March 2021
+//Purpose:To store information of a student using union
+#include <stdio.h>//Pre-process directive to include standard input and output functions header file
+#include <string.h>//Preprocessive directive to include string header file 
  
-union student 
-{
-            char name[20];
-            char subject[20];
-            float percentage;
-};
+union student {//union body starts
+            char name[20];//members name array
+            char subject[20];//member subject array
+            float percentage;//member percentage
+};//union body ends
  
-int main() 
-{
-    union student record1;
-    union student record2;
+int main() {//main function body starts
+    union student record1;//declaration 
+    union student record2;//declaration
  
     // assigning values to record1 union variable
-       strcpy(record1.name, "Raju");
-       strcpy(record1.subject, "Maths");
-       record1.percentage = 86.50;
+       strcpy(record1.name, "Raju");//using strcpy to store raju in record1.name
+       strcpy(record1.subject, "Maths");//using strcpy ro store math in record1.subject
+       record1.percentage = 86.50;//assignment
  
-       printf("Union record1 values example\n");
-       printf(" Name       : %s \n", record1.name);
-       printf(" Subject    : %s \n", record1.subject);
-       printf(" Percentage : %f \n\n", record1.percentage);
+       printf("Union record1 values example\n");//printf calling to print Union record1 values example
+       printf(" Name       : %s \n", record1.name);//printf calling to print Name   
+       printf(" Subject    : %s \n", record1.subject);//printf calling to print Subject   
+       printf(" Percentage : %f \n\n", record1.percentage);//printf calling to print Percentage
  
     // assigning values to record2 union variable
-       printf("Union record2 values example\n");
-       strcpy(record2.name, "Mani");
-       printf(" Name       : %s \n", record2.name);
+       printf("Union record2 values example\n");//printf calling to print Union record2 values example
+       strcpy(record2.name, "Mani");//using strcpy to store mani in record2.name
+       printf(" Name       : %s \n", record2.name);//printf calling to print Name 
  
-       strcpy(record2.subject, "Physics");
-       printf(" Subject    : %s \n", record2.subject);
+       strcpy(record2.subject, "Physics");//using strcpy to store record2.subject
+       printf(" Subject    : %s \n", record2.subject);//printf calling to print Subject
  
-       record2.percentage = 99.50;
-       printf(" Percentage : %f \n", record2.percentage);
-       return 0;
-}
+       record2.percentage = 99.50;//value assignment
+       printf(" Percentage : %f \n", record2.percentage);//printf calling to print Percentage 
+       return 0;//Return statement
+}//end of main function body
