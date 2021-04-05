2071834fccde4deafc29943953fd2ffbee31702c
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 11:32:01 +0530

    file is added

--- 16_MaximumElementIndexArr.c
+++ 16_MaximumElementIndexArr.c
@@ -1,23 +1,23 @@
-//A C Program to find the largest number in a user entered array.
-#include<stdio.h>
-int main(){
-	int i;
-	float arr[5];
+//Author name: HRITHIK KOUL
+//Creation date:21-03-2021
+//purpose: To find max element and index in array
+#include<stdio.h>//Preprocessor directive to include input output function header file
+int main(){//Start of the main function body
+	int i;///Declaring variable of integer data type
+	float arr[5];//Declaring of array of float data type
 
-	printf("Please enter five numbers:\n ");
+	printf("Please enter five numbers:\n ");//Printf function calling to print the five number
 
-	for (i = 0; i < 5; ++i) //Using for loop.
-	{
-    scanf("%f", &arr[i]);//Scanning the user entered values.
-	}
+	for (i = 0; i < 5; ++i){//start of for loop body
+    scanf("%f", &arr[i]);//Scanf function calling to input the value given by user
+	}//end of body of for loop
 
-		for (i = 1; i < 5; ++i)
+		for (i = 1; i < 5; ++i)//for loop
 		{
-			//Determing the largest value.
-    	if (arr[0] < arr[i])
+    	if (arr[0] < arr[i])//if statement
         	arr[0] = arr[i];
 		}
-	printf("Largest element = %.2f", arr[0]);//Printing the largest value.
+	printf("Largest element = %.2f", arr[0]);//printf function calling to print the largest element
 
-	return 0;
-}
+	return 0;//returning statement
+}//End of main function body
