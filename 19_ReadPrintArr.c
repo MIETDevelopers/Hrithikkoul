d5533d8405ad0ac7cfc8bc466ed8466117ceb824
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 11:44:52 +0530

    file is added

--- 19_ReadPrintArr.c
+++ 19_ReadPrintArr.c
@@ -1,25 +1,27 @@
-// A C Program to read and print elements in two dimensional array. © Ishav Verma 19/March/2021
-#include<stdio.h>
-int main(){
-   /* 2D array declaration*/
-   int disp[3][4];
-   /*Counter variables for the loop*/
-   int i, j;
-   for(i=0; i<3; i++) {
-      for(j=0;j<4;j++) {
-         printf("Enter value for disp[%d][%d]:", i, j);
-         scanf("%d", &disp[i][j]);
+//Author name : Hrithik koul
+//Creation date: 20-03-2021
+//Purpose :To read and print elements in two dimensional array 
+#include<stdio.h>//Preprocessive directive to include input output functions header file
+int main(){//Start of the main body function
+   int disp[2][3];//2D array declaration
+   int i, j;//variable declaration of integer type
+   //start of for loop
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);//printf function calling to print the value for disp
+         scanf("%d", &disp[i][j]);//Scanf function calling to input the value given by user
       }
    }
    //Displaying array elements
-   printf("Two Dimensional array elements:\n");
-   for(i=0; i<3; i++) {
-      for(j=0;j<4;j++) {
-         printf("%d \n", disp[i][j]);
-         if(j==4){
-            printf("\n");
+   printf("Two Dimensional array elements:\n");//printf function calling to print the two dimensional array elements
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("%d ", disp[i][j]);//printf function calling 
+         //if statement
+         if(j==2){
+            printf("\n");//printf function calling to print the number 
          }
       }
    }
-   return 0;
-} 
+   return 0;//return statement
+} //end of the main body function
