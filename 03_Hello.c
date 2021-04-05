933f0c44449cbb162c42cdbd80fac8d31cdfbf68
hrithikkoul <76619274+hrit887@users.noreply.github.com>
2021-04-05 09:58:44 +0530

    Update 03_Hello.c

--- 03_Hello.c
+++ 03_Hello.c
@@ -1,8 +1,10 @@
-//A basic C Program to print Hello World.				© Ishav Verma 18/March/2021
+//Author Name: Hrithik koul
+//Creation Date:16-March-2021
+//Purpose:A basic C Program to print Hello World.				© Ishav Verma 18/March/2021
 //Start
 #include<stdio.h> //Including standard input ouput header files from library.
 int main(){ 
-	printf("Hello Buddy!!");//Here we entered the texted to be printed.
-	return 0;//Returning.
+	printf("Hello world!!");//Here we entered the texted to be printed.
+	return 0;//return statement
 } 
 //End 
